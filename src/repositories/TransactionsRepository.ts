b99b2731efe71c9588da899f3f888747f83c4899
brennowilliam <brennowilliam@gmail.com>
2020-04-29 15:44:52 -0300

    solution to the fundamentos node challenge

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,16 @@
+/* eslint-disable no-param-reassign */
 import Transaction from '../models/Transaction';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
@@ -14,15 +21,38 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balanceCalculation = this.transactions.reduce(
+      (balance: Balance, transaction: Transaction) => {
+        if (transaction.type === 'income') {
+          balance.income += transaction.value;
+        }
+
+        if (transaction.type === 'outcome') {
+          balance.outcome += transaction.value;
+        }
+
+        balance.total = balance.income - balance.outcome;
+        return balance;
+      },
+      { income: 0, outcome: 0, total: 0 },
+    );
+    return balanceCalculation;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const transaction = new Transaction({
+      title,
+      value,
+      type,
+    });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,42 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    const errors: string[] = [];
+
+    if (!title) {
+      errors.push('Title is required for this transaction');
+    }
+
+    if (!value) {
+      errors.push('Value is required for this transaction');
+    }
+
+    if (!title) {
+      errors.push('Title is required for this transaction');
+    }
+
+    const balance = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome') {
+      if (value > balance.total) {
+        errors.push(
+          'The value of this transaction is greater than your income',
+        );
+      }
+    }
+
+    if (errors.length > 0) {
+      throw new Error(...errors);
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
